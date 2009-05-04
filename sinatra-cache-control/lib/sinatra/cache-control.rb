23068bcdd64ba6c1749063983d9905efe9bb9091
W. Andrew Loe III <andrew@andrewloe.com>
2009-05-04 15:52:15 -0700

    Simple cache control method.

--- sinatra-cache-control/lib/sinatra/cache-control.rb
+++ sinatra-cache-control/lib/sinatra/cache-control.rb
@@ -1,1 +1,24 @@
 # Don't require sinatra, it should be required before the plugin
+
+module Sinatra
+  
+  module CacheControl
+    
+    ##
+    # cache_control :public => true, :max_age => 5
+    # Cache-Control=public, max-age=5
+    def cache_control(options)
+      parts = options.map do |k, v|
+        if v == true
+          k
+        else
+          "#{k.to_s.tr('_', '-')}=#{v}"
+        end
+      end
+      
+      headers['Cache-Control'] = parts.join(',')
+    end
+    
+  end
+  
+end
