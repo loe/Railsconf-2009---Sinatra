f35568d62827060c68c50b0354ec24983b7763d5
W. Andrew Loe III <andrew@andrewloe.com>
2009-05-04 16:10:32 -0700

    Enable Rack::Cache to work as our reverse proxy.

--- using-cache-control/using-cache-control.rb
+++ using-cache-control/using-cache-control.rb
@@ -3,6 +3,9 @@ $LOAD_PATH.unshift File.dirname(__FILE__) + '/../sinatra-cache-control/lib'
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/cache-control'
+require 'rack/cache'
+
+use Rack::Cache
 
 get '/' do
   cache_control :public => true, :max_age => 5
