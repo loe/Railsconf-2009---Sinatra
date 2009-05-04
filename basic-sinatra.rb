929134a1e0a7b89437b768d2a5872c7f9872f924
W. Andrew Loe III <andrew@andrewloe.com>
2009-05-04 14:49:28 -0700

    Sugar for setting the content type, your browser should prompt you to download.

--- basic-sinatra.rb
+++ basic-sinatra.rb
@@ -1,18 +1,8 @@
 require 'rubygems' # This is probably a bad idea, but we'll leave that argument for another day.
 require 'sinatra'
+require 'json'
 
 get '/' do
-  
-end
-
-post '/' do
-
-end
-
-put '/' do
-  
-end
-
-delete '/' do
-  
+  content_type 'application/json'
+  {'foo' => 'goo'}.to_json
 end
