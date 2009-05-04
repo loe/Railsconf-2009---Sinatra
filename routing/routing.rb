3065d138116a39557344be8794097b85d5639223
W. Andrew Loe III <andrew@andrewloe.com>
2009-05-04 16:42:57 -0700

    Capture an array of the sections of the uri passed in.

--- routing/routing.rb
+++ routing/routing.rb
@@ -1,15 +1,6 @@
 require 'rubygems'
 require 'sinatra/base'
 
-get '/admin/*' do
-  halt(401, "get out!") unless authenticated?
-  pass
-end
-
-get '/admin/:name' do
-  pass
-end
-
-get '/admin/super-top-secret/:name' do
-  
+get '/*' do
+  request.path_info.split('/')
 end
