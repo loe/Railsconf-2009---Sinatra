f800f14e0b091c562b38d03b8698ec4bc2ddbd3b
W. Andrew Loe III <andrew@andrewloe.com>
2009-05-04 14:45:18 -0700

    Sinatra won't touch your body, it will help you set your headers and status code.

--- basic-sinatra.rb
+++ basic-sinatra.rb
@@ -1,11 +1,6 @@
 require 'rubygems' # This is probably a bad idea, but we'll leave that argument for another day.
 require 'sinatra'
 
-not_found do
-  status(200)
-  "hi from other"
-end
-
-get '/foo' do
-  "hi from foo"
+get '/env' do
+  env.inspect
 end
