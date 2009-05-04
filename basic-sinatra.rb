828bf1b81930562a55992fd40913eb72fbef1dce
W. Andrew Loe III <andrew@andrewloe.com>
2009-05-04 14:48:12 -0700

    Each of these is a different route. These are tasty.

--- basic-sinatra.rb
+++ basic-sinatra.rb
@@ -1,6 +1,18 @@
 require 'rubygems' # This is probably a bad idea, but we'll leave that argument for another day.
 require 'sinatra'
 
-get '/env' do
-  env.inspect
+get '/' do
+  
+end
+
+post '/' do
+
+end
+
+put '/' do
+  
+end
+
+delete '/' do
+  
 end
