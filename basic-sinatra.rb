6fd50f5b70e957e51186089f6bf764fb813f2eb0
W. Andrew Loe III <andrew@andrewloe.com>
2009-05-04 14:42:40 -0700

    not_bound handler will pickup unmatched routes. status is sugar to set the status code so you don't have to manipulate hashes.

--- basic-sinatra.rb
+++ basic-sinatra.rb
@@ -1,6 +1,11 @@
 require 'rubygems' # This is probably a bad idea, but we'll leave that argument for another day.
 require 'sinatra'
 
+not_found do
+  status(200)
+  "hi from other"
+end
+
 get '/foo' do
   "hi from foo"
 end
